198c4eceaa7003bc735f7b715bfbc411044a4a73
chitresh99 <chitreshpoojary990@gmail.com>
2024-12-27 16:27:58 +0530

    init

---
+++ src/test/java/com/example/journalism/journalIt/JournalItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.journalism.journalIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
